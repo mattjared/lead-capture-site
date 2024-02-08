42b202b840d9c436435bbb28a4aed969ddabe454
Matt Jared <matt.jared@vercel.com>
2024-02-07 22:12:29 -0600

    Sylvester Gray

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <Analytics />
+      <Analytics />    
+      <SpeedInsights />
     </html>
   );
 }
